7863ea9951dc3f150902737d392deeeb824b2af5
Cheng Lian <lian.cs.zju@gmail.com>
2026-03-17 11:05:27 -0700

    chore: Add settings to init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@ vim.lsp.config["joule"] = {
 	cmd = { "joule", "serve" },
 	filetypes = { "jsonnet" },
 	root_markers = { "vendor", "jsonnetfile.json", ".git" },
+	settings = {
+		library_paths = { "**/vendor", "**/jsonnet" },
+		exclude = { "**/.*" },
+		include = { "**/*" },
+		suffixes = { "jsonnet", "libsonnet" },
+	},
 }
 
 vim.lsp.enable("joule", true)
