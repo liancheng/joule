9627a08171dcf18ddf5b955ccf75fae54a3c7bb1
Cheng Lian <lian.cs.zju@gmail.com>
2026-03-17 12:44:20 -0700

    feat: Highlight tail expressions for functions, local bindings, and conditionals

--- src/joule/providers/document_highlight_provider.py
+++ src/joule/providers/document_highlight_provider.py
@@ -2,27 +2,38 @@ from itertools import chain
 
 import lsprotocol.types as L
 
-from joule.ast import AnalysisPhase, Document, Id
+from joule import ast as A
+from joule.ast import AnalysisPhase
 from joule.maybe import maybe
 
 
 class DocumentHighlightProvider:
-    def serve(self, tree: Document, pos: L.Position) -> list[L.DocumentHighlight]:
+    def serve(self, tree: A.Document, pos: L.Position) -> list[L.DocumentHighlight]:
         assert tree.analysis_phase == AnalysisPhase.ScopeResolved
         from lsprotocol.types import DocumentHighlightKind as K
 
-        match tree.node_at(pos):
-            case Id.Var() as var:
-                to_highlight = chain(var.references, [var])
-            case Id.VarRef() as ref:
+        match node := tree.node_at(pos):
+            case A.Field():
+                match value := node.value:
+                    case A.Fn():
+                        to_highlight = value.body.tails
+                    case _:
+                        to_highlight = value.tails
+            case A.Fn():
+                to_highlight = node.body.tails
+            case A.Id.Var():
+                to_highlight = chain(node.references, [node])
+            case A.Id.VarRef():
                 to_highlight = (
-                    id for var in maybe(ref.var) for id in chain(var.references, [var])
+                    id for var in maybe(node.var) for id in chain(var.references, [var])
                 )
+            case A.If() | A.Local():
+                to_highlight = node.tails
             case _:
                 to_highlight = ()
 
         return [
             L.DocumentHighlight(id.location.range, kind)
             for id in to_highlight
-            if (kind := K.Write if isinstance(id, Id.Var) else K.Read,)
+            if (kind := K.Write if isinstance(id, A.Id.Var) else K.Read,)
         ]
