bc153107409d63973178b755dd26fbae9cc5f31f
Cheng Lian <lian.cs.zju@gmail.com>
2026-03-27 11:31:17 -0700

    refactor: Move initialization logic into `JouleLanguageServer`

--- src/joule/server.py
+++ src/joule/server.py
@@ -1,7 +1,7 @@
 import asyncio
 import uuid
 from enum import IntEnum, auto
-from functools import cached_property
+from functools import cached_property, partial
 from pathlib import Path
 
 import lsprotocol.types as L
@@ -30,29 +30,126 @@ class JouleLanguageServer(LanguageServer):
 
     state: State = State.Created
 
+    @property
+    def workspace_uri(self) -> URI:
+        assert self.state in [self.State.Initializing, self.State.Initialized]
+
+        match list(self.workspace.folders.keys()):
+            case [_, _, *_]:
+                raise RuntimeError("Joule does not support multi-workspace.")
+            case [uri]:
+                return uri
+            case _ if (uri := self.workspace.root_uri) is not None:
+                return Path.from_uri(uri).resolve().as_uri()
+            case _ if (path := self.workspace.root_path) is not None:
+                return Path(path).resolve().as_uri()
+            case _:
+                raise RuntimeError("No valid workspace URI detected.")
+
+    @property
+    def workspace_path(self) -> Path:
+        return Path.from_uri(self.workspace_uri)
+
     _config: JouleConfig | None = None
 
     @property
     def config(self) -> JouleConfig:
-        assert self.state == self.State.Initialized and self._config is not None
+        assert self.state == self.State.Initialized
+        assert self._config is not None
         return self._config
 
-    @config.setter
-    def config(self, value: JouleConfig):
+    async def load_config(self) -> JouleConfig:
         assert self.state == self.State.Initializing
-        self._config = value
+
+        items = [
+            L.ConfigurationItem(section=field)
+            for field in JouleConfig.model_fields.keys()
+        ]
+
+        params = L.ConfigurationParams(items)
+        values = await self.workspace_configuration_async(params)
+
+        self._config = JouleConfig(
+            **{
+                section: config
+                for section, config in zip(JouleConfig.model_fields.keys(), values)
+                if config is not None
+            }
+        )
+
+        return self._config
 
     _document_store: DocumentStore | None = None
 
     @property
     def document_store(self) -> DocumentStore:
-        assert self.state == self.State.Initialized and self._document_store is not None
+        assert self.state == self.State.Initialized
+        assert self._document_store is not None
         return self._document_store
 
-    @document_store.setter
-    def document_store(self, value: DocumentStore):
+    async def load_workspace(self):
         assert self.state == self.State.Initializing
-        self._document_store = value
+        assert self._config is not None
+
+        self._document_store = DocumentStore(self._config, self.workspace_uri)
+
+        progress_report_token = str(uuid.uuid4())
+        await self.work_done_progress.create_async(progress_report_token)
+        self.work_done_progress.begin(
+            progress_report_token,
+            L.WorkDoneProgressBegin(title="Loading workspace..."),
+        )
+
+        workspace_path = self.workspace_path
+        paths = list(self._document_store.enumerate(workspace_path))
+        processed = 0
+
+        def callback(path: Path):
+            nonlocal processed
+            processed += 1
+
+            self.work_done_progress.report(
+                progress_report_token,
+                L.WorkDoneProgressReport(
+                    percentage=min(int(processed * 100.0 / len(paths)), 100),
+                    message=path.relative_to(workspace_path).as_posix(),
+                ),
+            )
+
+        await asyncio.to_thread(
+            partial(
+                self._document_store.load_workspace,
+                paths=paths,
+                callback=callback,
+            )
+        )
+
+        self.work_done_progress.end(progress_report_token, L.WorkDoneProgressEnd())
+
+    async def register_capabilities(self):
+        assert self.state == self.State.Initializing
+        assert self._config is not None
+
+        watchers = [
+            L.FileSystemWatcher(
+                glob_pattern=L.RelativePattern(
+                    base_uri=folder,
+                    pattern=f"**/*.{{{','.join(self._config.suffixes)}}}",
+                ),
+                kind=L.WatchKind.Change | L.WatchKind.Create | L.WatchKind.Delete,
+            )
+            for folder in self.workspace.folders.values()
+        ]
+
+        registrations = [
+            L.Registration(
+                id=str(uuid.uuid4()),
+                method=L.WORKSPACE_DID_CHANGE_WATCHED_FILES,
+                register_options=L.DidChangeWatchedFilesRegistrationOptions(watchers),
+            )
+        ]
+
+        await self.client_register_capability_async(L.RegistrationParams(registrations))
 
     @cached_property
     def loader(self) -> DocumentLoader:
@@ -82,92 +179,10 @@ server = JouleLanguageServer("joule", "v0.1")
 async def initialized(ls: JouleLanguageServer, _: L.InitializedParams):
     ls.state = JouleLanguageServer.State.Initializing
 
-    config_values = await ls.workspace_configuration_async(
-        L.ConfigurationParams(
-            [L.ConfigurationItem(section=field) for field in JouleConfig.model_fields]
-        )
-    )
+    await ls.load_config()
+    await ls.load_workspace()
+    await ls.register_capabilities()
 
-    config = JouleConfig(
-        **{
-            section: config
-            for section, config in zip(JouleConfig.model_fields, config_values)
-            if config is not None
-        }
-    )
-
-    match list(ls.workspace.folders.keys()):
-        case [_, _, *_]:
-            raise RuntimeError("Joule does not support multi-workspace.")
-        case [uri]:
-            workspace_uri = uri
-        case _ if (uri := ls.workspace.root_uri) is not None:
-            workspace_uri = Path.from_uri(uri).resolve().as_uri()
-        case _ if (path := ls.workspace.root_path) is not None:
-            workspace_uri = Path(path).resolve().as_uri()
-        case _:
-            raise RuntimeError("No valid workspace URI detected.")
-
-    workspace_path = Path.from_uri(workspace_uri)
-    document_store = DocumentStore(config, workspace_uri)
-
-    token = str(uuid.uuid4())
-    await ls.work_done_progress.create_async(token)
-
-    ls.work_done_progress.begin(
-        token,
-        L.WorkDoneProgressBegin(
-            title="Loading workspace...",
-            cancellable=True,
-            percentage=0,
-        ),
-    )
-
-    def load_workspace():
-        paths = list(document_store.enumerate(workspace_path))
-        total = len(paths)
-        processed = 0
-
-        def callback(_: Path):
-            nonlocal processed
-
-            processed += 1
-            percentage = min(int(processed * 100.0 / total), 100)
-
-            ls.work_done_progress.report(
-                token,
-                L.WorkDoneProgressReport(percentage=percentage),
-            )
-
-        document_store.load_workspace(paths, callback)
-
-    await asyncio.to_thread(load_workspace)
-    ls.work_done_progress.end(token, L.WorkDoneProgressEnd())
-
-    async def register_capabilities():
-        watchers = [
-            L.FileSystemWatcher(
-                glob_pattern=L.RelativePattern(
-                    base_uri=folder,
-                    pattern=f"**/*.{{{','.join(config.suffixes)}}}",
-                ),
-                kind=L.WatchKind.Change | L.WatchKind.Create | L.WatchKind.Delete,
-            )
-            for folder in ls.workspace.folders.values()
-        ]
-
-        registration = L.Registration(
-            id=str(uuid.uuid4()),
-            method=L.WORKSPACE_DID_CHANGE_WATCHED_FILES,
-            register_options=L.DidChangeWatchedFilesRegistrationOptions(watchers),
-        )
-
-        await ls.client_register_capability_async(L.RegistrationParams([registration]))
-
-    await register_capabilities()
-
-    ls.config = config
-    ls.document_store = document_store
     ls.state = JouleLanguageServer.State.Initialized
 
 
